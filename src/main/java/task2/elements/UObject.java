1a47a4bcd4ba77d70474d3e172cfe5fe590354e7
valeria.golovko <valeria.golovko@megafon.ru>
2022-01-22 22:43:00 +0400

    rewrote test with mocks

--- src/main/java/task2/elements/UObject.java
+++ src/main/java/task2/elements/UObject.java
@@ -1,11 +1,7 @@
 package task2.elements;
 
-import java.util.Map;
-
 public interface UObject<T> {
     T getProperty(Class<T> cls, String propertyName);
 
     void setProperty(String propertyName, T newValue);
-
-    Map<String, T> getProperties();
 }
